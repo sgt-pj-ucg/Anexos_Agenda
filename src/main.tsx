d7b67d0b4de201652990c41bf8fa68414af27e94
sgt-pj-ucg <carlos.olave85@gmail.com>
2026-09-01 21:05:29 -0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { AccessGate } from './components/AccessGate.tsx'
 import { PasswordGate } from './components/PasswordGate.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <PasswordGate>
-      <App />
-    </PasswordGate>
+    <AccessGate>
+      <PasswordGate>
+        <App />
+      </PasswordGate>
+    </AccessGate>
   </StrictMode>,
 )
 
